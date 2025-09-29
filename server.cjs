b73dd3d1f80b536b3983675098ee975e90b42f54
eslctstore-hash <e.slct.store@gmail.com>
2025-09-29 22:32:59 +0400

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,8 +1,8 @@
 // server.cjs
 
-import express from "express";
-import axios from "axios";
-import bodyParser from "body-parser";
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json());
